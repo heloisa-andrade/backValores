b339716a7471f8861bf2b206b22151caac9e0729
heloisa-andrade <heloisa.andrade@picpay.com>
2025-03-20 17:53:57 -0300

    algo

--- jogoVivi/src/main/java/com/example/jogoVivi/Controller/ValoresController.java
+++ jogoVivi/src/main/java/com/example/jogoVivi/Controller/ValoresController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*") // Permite apenas esse domínio
 public class ValoresController {
     @Autowired
     ValoresRepositry valores;
